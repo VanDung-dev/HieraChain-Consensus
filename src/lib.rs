9b48574397630f1a8b4ecce9f6884e777ccd46f7
Văn Dũng <131602164+VanDung-dev@users.noreply.github.com>
2025-12-27 11:36:25 +0700

    chore(LIB): bind PySubChain class to Python module

--- src/lib.rs
+++ src/lib.rs
@@ -762,6 +762,7 @@ fn hierachain_consensus(_py: Python, m: &Bound<PyModule>) -> PyResult<()> {
     m.add_class::<crate::core::block::Block>()?;
     m.add_class::<crate::core::blockchain::PyBlockchain>()?;
     m.add_class::<crate::hierarchical::main_chain::PyMainChain>()?;
+    m.add_class::<crate::hierarchical::sub_chain::PySubChain>()?;
     m.add_class::<PyProofOfFederation>()?;
 
     // Add BFT consensus classes
